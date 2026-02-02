40adc12ad1e02d0861567b6f8651820191ab9e79
Aaryan Purohit <paaryan2005@gmail.com>
2026-02-02 10:09:19 +0530

    Initial project setup with frontend and backend

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,42 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+
+const router = express.Router();
+
+/* Register */
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = new User({
+    username,
+    email,
+    password: hashedPassword
+  });
+
+  await user.save();
+  res.json({ message: "User Registered" });
+});
+
+/* Login */
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return res.status(400).json({ msg: "User not found" });
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) return res.status(400).json({ msg: "Wrong password" });
+
+  const token = jwt.sign(
+    { id: user._id },
+    process.env.JWT_SECRET
+  );
+
+  res.json({ token });
+});
+
+export default router;
